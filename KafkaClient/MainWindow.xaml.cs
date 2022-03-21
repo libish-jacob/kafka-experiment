065e083113679dc86ef43d3f21e5714a70db53c9
Libish Jacob <jajac@vestas.com>
2022-03-21 22:10:00 +0530

    Initial commit
    Initial commit

---
+++ KafkaClient/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KafkaClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(() => Receive());
+            Task.Factory.StartNew(()=>Send());
+
+        }
+
+        private void Receive()
+        {
+            var config = new ConsumerConfig
+            {
+                // two brokers on two different ports are running on localhost.
+                BootstrapServers = "localhost:9092,localhost:9192",
+                GroupId = "foo",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe(new List<string>() { "testotherdata" });
+                var count = 5;
+                while (count > 0)
+                {
+                    var res = consumer.Consume(50000);
+                    Console.WriteLine(res.Value);
+                    count--;
+                }
+            }
+        }
+
+        private async void Send()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                ClientId = Dns.GetHostName(),
+            };
+
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var count = 5;
+                while (count > 0)
+                {
+                    var tsk = await producer.ProduceAsync("testotherdata", new Message<Null, string>() { Value = $"message {Guid.NewGuid()}" });
+                    if (tsk.Status == PersistenceStatus.Persisted)
+                    {
+                        Console.WriteLine("Sent!");
+                    }
+
+                    count--;
+                }
+            }
+        }
+    }
+}
